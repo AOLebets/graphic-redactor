1f9555458b585ddf1158e9414bb3d4d6fde767a5
AALebets <89778658+AALebets@users.noreply.github.com>
2022-08-08 00:33:43 +0300

    Delete Main.java

--- Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        ShapePrinter sPrinter = new ShapePrinter();
-        Square sq = new Square();
-        sPrinter.printShape(sq);
-    }
-}
